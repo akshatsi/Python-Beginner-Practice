f12f3e994955625951d4b52a0f9d4746e9f59dd5
Akshat Singh <301akshat@gmail.com>
2025-12-30 21:54:05 +0530

    Another commit

--- second_largest.py
+++ second_largest.py
@@ -1,13 +1,14 @@
-def largest_element(a):
+def second_largest(a):
     x = a[0]
-
+    y = 0
     for i in range (1, len(a)):
         if a[i] > x:
+            y = x
             x = a[i]
         else:
             continue
-    return x
 
+    return y
 a = [101,89,93,32,1,45,98, 1005, 23]
-print("largest element is ", largest_element(a))
-
+print("second largest number is:", second_largest(a))
+        
